6adc1424d41df8e91951244ed39502a5ca51a199
lowercaselife <dansonkalaghe@gmail.com>
2022-06-09 14:32:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple application to practice on Async/Await
+
+## GitHub Profile finder
+
+### This aplication queries the github api when you search for a valid user on GitHub and displays the profile information.
---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "b1280545ac26fd7744ba";
+    this.client_secret = "be10b349dc75338e3fbd96cce7f0552703e76911";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
